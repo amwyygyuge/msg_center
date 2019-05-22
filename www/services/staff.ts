36d3623614bb75199e185e4757ddbc930abda459
蔡俊雄 <junxiong.cai186@gmail.com>
2019-05-22 14:21:47 +0800

    bugfix

--- www/services/staff.ts
+++ www/services/staff.ts
@@ -1,3 +1,4 @@
-import { request } from './../utils/domain'
+// import { request } from './../utils/domain'
 import { RequestInit } from 'node-fetch'
-export const queryStaff = (init: RequestInit) => request('/staff/query', init)
+// export const queryStaff = (init: RequestInit) => request('/staff/query', init)
+export const queryStaff = (_init: RequestInit) => Promise.resolve([])
